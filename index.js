bf4aadc086c7844b10908008d57769d2e47a1b61
Meghraj <meghraj.deshmukh103@gmail.com>
2022-04-11 16:28:52 +0530

    completed the challenge

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const style={
     chessbox_view_container:"chessbox-view-container",
     box:"box",
     bcBlack:"bc-black",
-    bcWhite:"bc-white"
+    bcWhite:"bc-white",
+    bcRed:"bc-red",
 }
 const CHESS_SIZE=8;
-const chessBox=[CHESS_SIZE][CHESS_SIZE];
+let diagonal=[];
+let chessBox=Create2DArray(CHESS_SIZE);
 
 /**Self invoke function */
   (function(){
@@ -19,15 +21,37 @@ const chessBox=[CHESS_SIZE][CHESS_SIZE];
 
   })()
 
+
+  function Create2DArray(rows) {
+    var arr = [];
+  
+    for (var i=0;i<rows;i++) {
+       arr[i] = [];
+    }
+  
+    return arr;
+  }
+
 /**return the Box div */
 function makeBox(){
-
+   
+  
     try{
 
     const chessViewContainer= createElement(div);
     
         chessViewContainer.classList.add(style.chessbox_view_container);
-         
+       
+
+         chessViewContainer.addEventListener('click',(event)=>{
+             console.log("Clicked..... box",event);
+             event.stopPropagation();
+             const id=event.target.id;
+             console.log("id",id);
+             const [row,column]=id.split("@");
+             getDiagonal(row,column);
+
+         })
           for(let i=0;i<CHESS_SIZE;i++){
             const row=[];
               for(let j=0;j<CHESS_SIZE;j++){
@@ -35,8 +59,11 @@ function makeBox(){
                 box.classList.add(style.box);
                 box.classList.add(getStyle(i,j));
                // row.push(box);
-               chessBox[i][j]=box;
+                  console.log("chessbox",chessBox);
+                  chessBox[i][j]=box;
+                 box.id=i+"@"+j;
                 chessViewContainer.appendChild(box);
+            
               }
             // chessViewContainer.appendChild(...row);
              
@@ -44,6 +71,7 @@ function makeBox(){
           
          
       return chessViewContainer;
+
     }catch(error){
         
         console.error("ERROR",error);
@@ -79,36 +107,76 @@ function createElement(type){
 
 /**getDiagonal array*/
 function getDiagonal(row,column){
-   const diagonal=[];
+   
+   console.log("Row,column",row,column);
    /**row-1 - right and left */
    //Upper side
+   clearUI();
+
    //right
-   let tempCol=column+1;
-    for(const i=row-1;i>=0;i--){
-       diagonal.push(chessBox[i][tempCol]);
+  let  _column=parseInt(column);
+   let _row=parseInt(row);
+
+   let tempCol=_column+1;
+    for(let i=_row-1;i>=0;i--){
+     
+      if(getRef(i,tempCol)){
+        diagonal.push(chessBox[i][tempCol]);
+      }
        tempCol++;
     }
     //left
-      tempCol=column;
-    for(const i=row-1;i>=0;i--){
+      tempCol=_column-1;
+    for(let i=row-1;i>=0;i--){
+     
+      if(getRef(i,tempCol)){
         diagonal.push(chessBox[i][tempCol]);
+      }
+        
         tempCol--;
     }
     
-
+console.log("upper ",diagonal);
     //Down
 
      //right
-     tempCol=column+1;
-   for(const i=row+1;i;i++){
+     tempCol=_column+1;
+   for(let i=_row+1;i<CHESS_SIZE;i++){
+    console.log("chess ",i,"---",tempCol,"checbox=  ",chessBox[i][tempCol]);
+    if(getRef(i,tempCol)){
       diagonal.push(chessBox[i][tempCol]);
+    }
       tempCol++;
    }
    //left
-     tempCol=column;
-   for(const i=row-1;i>=0;i++){
-       diagonal.push(chessBox[i][tempCol]);
+     tempCol=_column-1;
+   for(let  i=_row+1;i<CHESS_SIZE;i++){
+    if(getRef(i,tempCol)){
+      diagonal.push(chessBox[i][tempCol]);
+    }
        tempCol--;
    }
    
+
+   console.log("Diagpnal array", diagonal);
+   diagonal.push(chessBox[row][column])
+   diagonal.forEach((box)=>{
+    box.classList.add(style.bcRed)
+   })
+}
+
+function getRef(row,column){
+
+  return chessBox[row][column];
+}
+
+function clearUI(){
+console.log("clear UI");
+  diagonal.forEach((box)=>{
+    console.log("cleaning", box);
+    
+    box.classList.remove(style.bcRed)
+   });
+   diagonal=[];
+  
 }
