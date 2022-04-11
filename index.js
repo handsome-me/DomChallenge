beed3f4b6ac820016c14ee82e030c9266ef80be4
Meghraj <meghraj.deshmukh103@gmail.com>
2022-04-11 15:36:58 +0530

    diagonal logic created

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const root=document.getElementById("root");
+
 const ERROR_TYPE_NOT_FOUND="ERROR_TYPE_NOT_FOUND";
 const div="div";
 const style={
-    chessbox_view_container:"chessbox-view-container"
+    chessbox_view_container:"chessbox-view-container",
+    box:"box",
+    bcBlack:"bc-black",
+    bcWhite:"bc-white"
 }
 const CHESS_SIZE=8;
+const chessBox=[CHESS_SIZE][CHESS_SIZE];
 
 /**Self invoke function */
   (function(){
-
-   root.appendChild()
+  console.log("root",root);
+  console.log("makeBox", makeBox());
+   root.appendChild(makeBox())
 
   })()
 
@@ -21,10 +27,25 @@ function makeBox(){
     const chessViewContainer= createElement(div);
     
         chessViewContainer.classList.add(style.chessbox_view_container);
-        
-        
-
+         
+          for(let i=0;i<CHESS_SIZE;i++){
+            const row=[];
+              for(let j=0;j<CHESS_SIZE;j++){
+                const box=createElement(div);
+                box.classList.add(style.box);
+                box.classList.add(getStyle(i,j));
+               // row.push(box);
+               chessBox[i][j]=box;
+                chessViewContainer.appendChild(box);
+              }
+            // chessViewContainer.appendChild(...row);
+             
+          }
+          
+         
+      return chessViewContainer;
     }catch(error){
+        
         console.error("ERROR",error);
 
     }
@@ -32,6 +53,16 @@ function makeBox(){
 
 }
 
+function getStyle(row,column){
+
+    if(row%2==0){
+   return column%2==0?style.bcWhite:style.bcBlack
+    }else{
+
+    }
+    return column%2==0?style.bcBlack:style.bcWhite;
+
+}
 
 /**Make the element -(div, p or any other) */
 function createElement(type){
@@ -43,4 +74,41 @@ function createElement(type){
 
   throw ERROR_TYPE_NOT_FOUND;
 
+}
+
+
+/**getDiagonal array*/
+function getDiagonal(row,column){
+   const diagonal=[];
+   /**row-1 - right and left */
+   //Upper side
+   //right
+   let tempCol=column+1;
+    for(const i=row-1;i>=0;i--){
+       diagonal.push(chessBox[i][tempCol]);
+       tempCol++;
+    }
+    //left
+      tempCol=column;
+    for(const i=row-1;i>=0;i--){
+        diagonal.push(chessBox[i][tempCol]);
+        tempCol--;
+    }
+    
+
+    //Down
+
+     //right
+     tempCol=column+1;
+   for(const i=row+1;i;i++){
+      diagonal.push(chessBox[i][tempCol]);
+      tempCol++;
+   }
+   //left
+     tempCol=column;
+   for(const i=row-1;i>=0;i++){
+       diagonal.push(chessBox[i][tempCol]);
+       tempCol--;
+   }
+   
 }
