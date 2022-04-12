e5f4ac4bba19ecb3179bea26a78e76a5638d8455
Meghraj <meghraj.deshmukh103@gmail.com>
2022-04-12 22:44:30 +0530

    grid view created

--- Pixelboard.js
+++ Pixelboard.js
@@ -5,4 +5,42 @@ const colors={
     green:'green',
     orange:'orange',
     yellow:'yellow'
+};
+const div="div";
+const root=document.getElementById("root");
+
+const style={
+  
+    gridView:"grid",
+
 }
+
+(function(){
+    renderView(root);
+})();
+
+function renderView(root){
+   
+  const element=  createElement(div);
+
+  element.classList.add(style.gridView);
+  
+  
+   
+
+  root.appendChile(root);
+}
+ 
+
+/**Make the element -(div, p or any other) */
+function createElement(type){
+   
+    const element= document.createElement(type);
+   
+      if(element)
+     return element;
+   
+     throw ERROR_TYPE_NOT_FOUND;
+   
+   }
+   
