432c1b2046f6e433f454c7ea5b229e5d6a00739d
Meghraj <meghraj.deshmukh103@gmail.com>
2022-04-13 15:23:09 +0530

    event function

--- Pixelboard.js
+++ Pixelboard.js
@@ -12,20 +12,50 @@ const root=document.getElementById("root");
 const style={
   
     gridView:"grid",
+    gridBox:"gridBox"
 
 }
 
-(function(){
-    renderView(root);
-})();
+// (
+//     function(){
+
+//     renderView(root);
+
+
+// })();
+function onClick(event){
+
+
+console.log("event target",event.currentTarget);
+
+}
+
+renderView(root);
 
 function renderView(root){
+
+
    
+
+    root.classList.add(style.gridView);
+   console.log("renderView is called");
+
   const element=  createElement(div);
 
   element.classList.add(style.gridView); 
+  element.addEventListener('click',onClick);
+    //"mouseenter
+    element.addEventListener('mouseover',onClick);
+   for(let i=0;i<20;i++){
+    
+     const element_box=createElement(div);
+     element_box.classList.add(style.gridBox);
+    
+     element.appendChild(element_box);
 
-  root.appendChile(root);
+   }
+
+  root.appendChild(element);
 
 }
  
